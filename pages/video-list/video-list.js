20350a39214bbef08c3b37dc071cbc29d8f8cae1
yudanjing <eoin@live.in>
2022-07-05 23:42:09 +0800

    Fix：第一次提交

---
+++ pages/video-list/video-list.js
@@ -0,0 +1,79 @@
+// pages/video-list/video-list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    videoList: [
+      {
+        "typeid": 1,
+        "videoimg": "../../images/p002.png",
+        "videoname": "测试视频1",
+        "videourl": "https://media.w3.org/2010/05/sintel/trailer.mp4"
+      },
+      {
+        "typeid": 2,
+        "videoimg": "../../images/p002.png",
+        "videoname": "测试视频2",
+        "videourl": "https://educate-video-101-baidu.babybus.com/202207051656/dfb89e1373e6c20f21028a7c669d85d6/Educate/PepInfoFile/20220214/54b607f8329947b48d5ae6a6c0a16c3c.mp4"
+      },
+     ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
